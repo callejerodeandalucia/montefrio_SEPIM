4e252533a02f13f1d3010d5508ad33f780b89877
Javier G. Leal <jgleal@gmail.com>
2015-11-18 14:23:15 +0100

    migración de SEPIM

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
+url = "http://www.callejerodeandalucia.es/sepim/api";
 urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/search";
 urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/suggest";
 idAplicacion = 33;
